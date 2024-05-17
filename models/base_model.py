79fb75b7d2e13aca1da6e32830f726ab78ec3861
HichamBC <yours.hichambc@gmail.com>
2024-05-17 19:29:59 +0100

    recreating an instance with the same id feature

--- models/base_model.py
+++ models/base_model.py
@@ -4,13 +4,20 @@ import uuid
 
 class BaseModel:
 
-    def __init__(self, id, created_at, updated_at):
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now
-
-    def __str__(self):
-        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
+    def __init__(self, *args, **kwargs):
+        if kwargs:
+            for key, value in kwargs.items():
+                if key != "__class__":
+                    setattr(self, key, value)
+            
+            if "created_at" in kwargs:
+                self.created_at = datetime.strptime(kwargs["created_at"], "%Y-%m-%dT%H:%M:%S.%f")
+            if "updated_at" in kwargs:
+                self.updated_at = datetime.strptime(kwargs["updated_at"], "%Y-%m-%dT%H:%M:%S.%f")
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def save(self):
         self.updated_at = datetime.now()
@@ -22,3 +29,6 @@ class BaseModel:
         dictionary["updated_at"] = self.updated_at.isoformat()
 
         return dictionary
+    
+    def __str__(self):
+        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
