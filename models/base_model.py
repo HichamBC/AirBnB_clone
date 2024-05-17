a2d32e35d05d748b57657f6fa3379c8917a9aaff
HichamBC <yours.hichambc@gmail.com>
2024-05-17 14:48:45 +0100

    models/base_model.py

---
+++ models/base_model.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+from datetime import datetime
+import uuid
+
+class BaseModel:
+
+    def __init__(self, id, created_at, updated_at):
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now
+
+    def __str__(self):
+        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
+
+    def save(self):
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        dictionary = self.__dict__.copy()
+        dictionary["__class__"] = self.__class__.__name__
+        dictionary["created_at"] = self.created_at.isoformat()
+        dictionary["updated_at"] = self.updated_at.isoformat()
+
+        return dictionary
